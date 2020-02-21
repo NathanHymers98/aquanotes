4cf428f1c8c1609058e6f3e6f513d334249c530e
Nathan Hymers <nathan.hymers@dgkc2hgcjv3x.wren.wrenkitchens.com>
2020-02-21 12:33:11 +0000

    Symfony Fundementals

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -21,14 +21,33 @@ class GenusController extends Controller
     public function showAction($genusName) // The controller function. This function must return a response object
     {
 
+        $funFact = 'Octopuses can change the color of their body in just *three-tenths* of a second!';
+
+        $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
+
+        $key = md5($funFact);
+        if ($cache->contains($key)) { // If the cache contains the fun fact string
+            $funFact = $cache->fetch($key); // Then the cache should go and fetch the string from the cache
+        } else { // If the cache does not contain the string
+            sleep(1);
+            $funFact = $this->get('markdown.parser') // Get the parser object and use it to parse the string
+                ->transform($funFact);
+            $cache->save($key, $funFact); // Save the parsed string to the cache
+        }
+
+        $funFact = $this->get('markdown.parser') // Getting the markdown parser object by calling the container method get() and using one of its methods to parse the $funFact string
+            ->transform($funFact);
+
         return $this->render('genus/show.html.twig', array( // Using the render() function which goes out to the templating service object and calls a method named renderResponse so we can just return the render function with the twig view we want to show
                                                                     // along with any variables we want to use on this view.
             'name' => $genusName, // Passing a name variable into twig that is set to $genusName. The 'name' part is what is used by twig, this format is the same throughout.
+            'funFact' => $funFact,
         ));
     }
 
+    // Setting the route and generating the URL to a specific route, I am doing this by giving this route a specific name
     /**
-     * @Route("/genus/{genusName}/notes")
+     * @Route("/genus/{genusName}/notes", name="genus_show_notes")
      * @Method("GET")
      */
     public function getNotesAction($genusName)
